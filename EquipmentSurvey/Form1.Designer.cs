01ab5ad8edd9da46974c75d5c714272fce3a69a0
Keola McColgan <kmccolgan@cetratechnology.com>
2020-01-18 12:46:59 -1000

    Add project files.

---
+++ EquipmentSurvey/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace EquipmentSurvey
+{
+    partial class frm_equipmentSurvey
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_hostname = new System.Windows.Forms.Label();
+            this.txt_hostName = new System.Windows.Forms.TextBox();
+            this.lbl_IPAddress = new System.Windows.Forms.Label();
+            this.txt_IPAddress = new System.Windows.Forms.TextBox();
+            this.lbl_MACAddress = new System.Windows.Forms.Label();
+            this.txt_MACAddress = new System.Windows.Forms.TextBox();
+            this.lbl_serialNumber = new System.Windows.Forms.Label();
+            this.txt_serialNumber = new System.Windows.Forms.TextBox();
+            this.lbl_category = new System.Windows.Forms.Label();
+            this.cbx_category = new System.Windows.Forms.ComboBox();
+            this.lbl_brand = new System.Windows.Forms.Label();
+            this.txt_brand = new System.Windows.Forms.TextBox();
+            this.lbl_model = new System.Windows.Forms.Label();
+            this.txt_model = new System.Windows.Forms.TextBox();
+            this.lbl_operatingSystem = new System.Windows.Forms.Label();
+            this.txt_operatingSystem = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_hostname
+            // 
+            this.lbl_hostname.AutoSize = true;
+            this.lbl_hostname.Location = new System.Drawing.Point(12, 9);
+            this.lbl_hostname.Name = "lbl_hostname";
+            this.lbl_hostname.Size = new System.Drawing.Size(60, 13);
+            this.lbl_hostname.TabIndex = 0;
+            this.lbl_hostname.Text = "Host Name";
+            // 
+            // txt_hostName
+            // 
+            this.txt_hostName.Location = new System.Drawing.Point(109, 6);
+            this.txt_hostName.Name = "txt_hostName";
+            this.txt_hostName.Size = new System.Drawing.Size(121, 20);
+            this.txt_hostName.TabIndex = 1;
+            // 
+            // lbl_IPAddress
+            // 
+            this.lbl_IPAddress.AutoSize = true;
+            this.lbl_IPAddress.Location = new System.Drawing.Point(12, 35);
+            this.lbl_IPAddress.Name = "lbl_IPAddress";
+            this.lbl_IPAddress.Size = new System.Drawing.Size(58, 13);
+            this.lbl_IPAddress.TabIndex = 2;
+            this.lbl_IPAddress.Text = "IP Address";
+            // 
+            // txt_IPAddress
+            // 
+            this.txt_IPAddress.Location = new System.Drawing.Point(109, 32);
+            this.txt_IPAddress.Name = "txt_IPAddress";
+            this.txt_IPAddress.Size = new System.Drawing.Size(121, 20);
+            this.txt_IPAddress.TabIndex = 3;
+            // 
+            // lbl_MACAddress
+            // 
+            this.lbl_MACAddress.AutoSize = true;
+            this.lbl_MACAddress.Location = new System.Drawing.Point(12, 61);
+            this.lbl_MACAddress.Name = "lbl_MACAddress";
+            this.lbl_MACAddress.Size = new System.Drawing.Size(71, 13);
+            this.lbl_MACAddress.TabIndex = 4;
+            this.lbl_MACAddress.Text = "MAC Address";
+            // 
+            // txt_MACAddress
+            // 
+            this.txt_MACAddress.Location = new System.Drawing.Point(109, 58);
+            this.txt_MACAddress.Name = "txt_MACAddress";
+            this.txt_MACAddress.Size = new System.Drawing.Size(121, 20);
+            this.txt_MACAddress.TabIndex = 5;
+            // 
+            // lbl_serialNumber
+            // 
+            this.lbl_serialNumber.AutoSize = true;
+            this.lbl_serialNumber.Location = new System.Drawing.Point(12, 87);
+            this.lbl_serialNumber.Name = "lbl_serialNumber";
+            this.lbl_serialNumber.Size = new System.Drawing.Size(73, 13);
+            this.lbl_serialNumber.TabIndex = 6;
+            this.lbl_serialNumber.Text = "Serial Number";
+            // 
+            // txt_serialNumber
+            // 
+            this.txt_serialNumber.Location = new System.Drawing.Point(109, 84);
+            this.txt_serialNumber.Name = "txt_serialNumber";
+            this.txt_serialNumber.Size = new System.Drawing.Size(121, 20);
+            this.txt_serialNumber.TabIndex = 7;
+            // 
+            // lbl_category
+            // 
+            this.lbl_category.AutoSize = true;
+            this.lbl_category.Location = new System.Drawing.Point(12, 113);
+            this.lbl_category.Name = "lbl_category";
+            this.lbl_category.Size = new System.Drawing.Size(49, 13);
+            this.lbl_category.TabIndex = 8;
+            this.lbl_category.Text = "Category";
+            // 
+            // cbx_category
+            // 
+            this.cbx_category.FormattingEnabled = true;
+            this.cbx_category.Location = new System.Drawing.Point(109, 113);
+            this.cbx_category.Name = "cbx_category";
+            this.cbx_category.Size = new System.Drawing.Size(121, 21);
+            this.cbx_category.TabIndex = 9;
+            // 
+            // lbl_brand
+            // 
+            this.lbl_brand.AutoSize = true;
+            this.lbl_brand.Location = new System.Drawing.Point(12, 140);
+            this.lbl_brand.Name = "lbl_brand";
+            this.lbl_brand.Size = new System.Drawing.Size(35, 13);
+            this.lbl_brand.TabIndex = 10;
+            this.lbl_brand.Text = "Brand";
+            // 
+            // txt_brand
+            // 
+            this.txt_brand.Location = new System.Drawing.Point(109, 137);
+            this.txt_brand.Name = "txt_brand";
+            this.txt_brand.Size = new System.Drawing.Size(121, 20);
+            this.txt_brand.TabIndex = 11;
+            // 
+            // lbl_model
+            // 
+            this.lbl_model.AutoSize = true;
+            this.lbl_model.Location = new System.Drawing.Point(12, 166);
+            this.lbl_model.Name = "lbl_model";
+            this.lbl_model.Size = new System.Drawing.Size(36, 13);
+            this.lbl_model.TabIndex = 12;
+            this.lbl_model.Text = "Model";
+            // 
+            // txt_model
+            // 
+            this.txt_model.Location = new System.Drawing.Point(109, 163);
+            this.txt_model.Name = "txt_model";
+            this.txt_model.Size = new System.Drawing.Size(121, 20);
+            this.txt_model.TabIndex = 13;
+            // 
+            // lbl_operatingSystem
+            // 
+            this.lbl_operatingSystem.AutoSize = true;
+            this.lbl_operatingSystem.Location = new System.Drawing.Point(13, 192);
+            this.lbl_operatingSystem.Name = "lbl_operatingSystem";
+            this.lbl_operatingSystem.Size = new System.Drawing.Size(90, 13);
+            this.lbl_operatingSystem.TabIndex = 14;
+            this.lbl_operatingSystem.Text = "Operating System";
+            // 
+            // txt_operatingSystem
+            // 
+            this.txt_operatingSystem.Location = new System.Drawing.Point(109, 189);
+            this.txt_operatingSystem.Name = "txt_operatingSystem";
+            this.txt_operatingSystem.Size = new System.Drawing.Size(121, 20);
+            this.txt_operatingSystem.TabIndex = 15;
+            // 
+            // frm_equipmentSurvey
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 550);
+            this.Controls.Add(this.txt_operatingSystem);
+            this.Controls.Add(this.lbl_operatingSystem);
+            this.Controls.Add(this.txt_model);
+            this.Controls.Add(this.lbl_model);
+            this.Controls.Add(this.txt_brand);
+            this.Controls.Add(this.lbl_brand);
+            this.Controls.Add(this.cbx_category);
+            this.Controls.Add(this.lbl_category);
+            this.Controls.Add(this.txt_serialNumber);
+            this.Controls.Add(this.lbl_serialNumber);
+            this.Controls.Add(this.txt_MACAddress);
+            this.Controls.Add(this.lbl_MACAddress);
+            this.Controls.Add(this.txt_IPAddress);
+            this.Controls.Add(this.lbl_IPAddress);
+            this.Controls.Add(this.txt_hostName);
+            this.Controls.Add(this.lbl_hostname);
+            this.Name = "frm_equipmentSurvey";
+            this.Text = "Equipment Survey";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_hostname;
+        private System.Windows.Forms.TextBox txt_hostName;
+        private System.Windows.Forms.Label lbl_IPAddress;
+        private System.Windows.Forms.TextBox txt_IPAddress;
+        private System.Windows.Forms.Label lbl_MACAddress;
+        private System.Windows.Forms.TextBox txt_MACAddress;
+        private System.Windows.Forms.Label lbl_serialNumber;
+        private System.Windows.Forms.TextBox txt_serialNumber;
+        private System.Windows.Forms.Label lbl_category;
+        private System.Windows.Forms.ComboBox cbx_category;
+        private System.Windows.Forms.Label lbl_brand;
+        private System.Windows.Forms.TextBox txt_brand;
+        private System.Windows.Forms.Label lbl_model;
+        private System.Windows.Forms.TextBox txt_model;
+        private System.Windows.Forms.Label lbl_operatingSystem;
+        private System.Windows.Forms.TextBox txt_operatingSystem;
+    }
+}
+
---
+++ EquipmentSurvey/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EquipmentSurvey
+{
+    public partial class frm_equipmentSurvey : Form
+    {
+        public frm_equipmentSurvey()
+        {
+            InitializeComponent();
+        }
+    }
+}
